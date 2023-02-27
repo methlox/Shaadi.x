3362fa20032178041f5073b606719ea7f5789e52
Siddhant <84025779+methlox@users.noreply.github.com>
2023-02-28 03:43:44 +0530

    jai mata di commit

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import Video from '../../Media/video.mp4'
+import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1,HeroP, HeroBtnWrapper, ArrowForward, ArrowRight } from './HeroElements'
+import { Button } from '../ButtonElement'
+
+
+const HeroSection = () => {
+
+  const [hover, setHover] = useState(false)
+
+  const onHover = () => {
+    setHover(!hover)
+  }
+
+  return (
+    <HeroContainer>
+        <HeroBg>
+            <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
+        </HeroBg>
+        <HeroContent>
+            <HeroH1>Debug</HeroH1>
+            <HeroP>Together</HeroP>
+            <HeroH1>Forever!</HeroH1>
+            <HeroBtnWrapper>
+                <Button to='signup' onMouseEnter={onHover} onMouseleave={onHover} >
+                    Get Started {hover ? <ArrowForward /> : <ArrowRight />}
+                </Button>
+            </HeroBtnWrapper>
+        </HeroContent>
+    </HeroContainer>
+  )
+}
+
+export default HeroSection
